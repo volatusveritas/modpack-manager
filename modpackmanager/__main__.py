18601a69c50619efea9f204d8148cb503066d9e3
Volatus <volatusCoder@outlook.com>
2022-06-23 22:24:20 -0300

    add ScrollingTextMenu to termui

--- modpackmanager/__main__.py
+++ modpackmanager/__main__.py
@@ -1,6 +1,3 @@
-import unicurses as curses
-import time
-
 import termui
 from termui import menus
 
@@ -23,14 +20,53 @@ def window_error_test() -> None:
         "delete modpack",
         "Delete an existing modpack"
     )
+    lyrics_menu = menus.ScrollingTextMenu(manager,
+        "lyrics",
+        [
+            "Eu vou rimando e você tem que responder",
+            "eu vou chupar a sua pica mano, e cê nem vai vê",
+            "mano aqui é só improviso o que que você vai fazer",
+            "vou fazer uma live, no câmera privê",
+            "agora, legal, eu gostei da sua levada",
+            "eu vou te dar uma mamada",
+            "só me responde uma coisa, isso é rima decorada?",
+            "meu cú é o Batman, seu pau é o Charada",
+            "Dahora, eu gostei, deu pra ver que é improviso",
+            "meu pau no seu ouvido",
+            "mas você não ganha de mim, então já te dou um aviso",
+            "EU VOU RIMANDO E VOCÊ TEM QUE RESPONDER",
+            "EU VOU CHUPAR A SUA PICA MANO, E CÊ NEM VAI VÊ",
+            "MANO AQUI É SÓ IMPROVISO O QUE QUE VOCÊ VAI FAZER",
+            "VOU FAZER UMA LIVE, NO CÂMERA PRIVÊ",
+            "AGORA, LEGAL, EU GOSTEI DA SUA LEVADA",
+            "EU VOU TE DAR UMA MAMADA",
+            "SÓ ME RESPONDE UMA COISA, ISSO É RIMA DECORADA?",
+            "MEU CÚ É O BATMAN, SEU PAU É O CHARADA",
+            "DAHORA, EU GOSTEI, DEU PRA VER QUE É IMPROVISO",
+            "MEU PAU NO SEU OUVIDO",
+            "MAS VOCÊ NÃO GANHA DE MIM, ENTÃO JÁ TE DOU UM AVISO",
+            "eu vou rimando e você tem que responder",
+            "eu vou chupar a sua pica mano, e cê nem vai vê",
+            "mano aqui é só improviso o que que você vai fazer",
+            "vou fazer uma live, no câmera privê",
+            "agora, legal, eu gostei da sua levada",
+            "eu vou te dar uma mamada",
+            "só me responde uma coisa, isso é rima decorada?",
+            "meu cú é o batman, seu pau é o charada",
+            "dahora, eu gostei, deu pra ver que é improviso",
+            "meu pau no seu ouvido",
+            "mas você não ganha de mim, então já te dou um aviso",
+        ]
+    )
 
     (main_menu
         .add_hop("n", "new modpack", new_menu)
         .add_hop("e", "edit modpack", edit_menu)
         .add_hop("d", "delete modpack", del_menu)
+        .add_hop("l", "lyrics", lyrics_menu)
     )
 
-    for menu in [new_menu, edit_menu, del_menu]:
+    for menu in [new_menu, edit_menu, del_menu, lyrics_menu]:
         menu.add_hop("b", "back to main menu", main_menu)
 
     manager.start(main_menu)
--- termui/menus.py
+++ termui/menus.py
@@ -2,6 +2,23 @@ from __future__ import annotations
 import unicurses as curses
 
 import termui
+from termui import constants
+
+
+class MenuManager:
+    def __init__(self) -> None:
+        self.next: Menu | None = None
+
+    def start(self, starting_menu: Menu) -> None:
+        starting_menu.display()
+
+        while self.next:
+            next = self.next
+            self.next = None
+            next.display()
+
+    def queue_menu(self, menu: Menu) -> None:
+        self.next = menu
 
 
 class Menu:
@@ -11,8 +28,10 @@ class Menu:
         self.hops = {}
 
     def add_hop(self, key: str, description: str, target: Menu) -> Menu:
-        if key == "q":
-            raise ValueError("Can not assign a hop to the quit key (q)")
+        if key == constants.QUIT_KEY:
+            raise ValueError(
+                f"Can not assign a hop to the quit key ({constants.QUIT_KEY})"
+            )
 
         self.hops[key] = {
             "description": description,
@@ -38,20 +57,25 @@ class Menu:
 
         curses.addstr("Hops\n", curses.color_pair(termui.SEC_TITLE_COLOR))
         for hop in self.hops:
-            curses.addstr(4*" " + "[")
+            curses.addstr(constants.SUB_PADDING*" " + "[")
             curses.addstr(hop, curses.color_pair(termui.SPECIAL_COLOR))
             curses.addstr("] " + self.hops[hop]["description"])
             curses.addch("\n")
+
+    def extra_input(self, key: str) -> bool: return True
 
     def handle_input(self) -> None:
         while True:
             key = curses.getkey().decode("utf8")
 
-            if key == "q":
+            if key == constants.QUIT_KEY:
                 break
             elif key in self.hops:
                 self.manager.queue_menu(self.hops[key]["target"])
                 break
+            else:
+                if not self.extra_input(key):
+                    break
 
     def display(self) -> None:
         curses.clear()
@@ -62,7 +86,7 @@ class Menu:
         self.render_content()
         self.render_hops()
         curses.addch("\n")
-        curses.addstr("Press [q] to quit")
+        curses.addstr(f"Press [{constants.QUIT_KEY}] to quit")
         curses.addch("\n")
 
         self.handle_input()
@@ -74,21 +98,52 @@ class PlainTextMenu(Menu):
         self.text = text
 
     def render_content(self) -> None:
-        curses.addstr(2*" " + self.text + "\n")
+        curses.addstr(constants.CONTENT_PADDING*" " + self.text + "\n")
         curses.addch("\n")
 
 
-class MenuManager:
-    def __init__(self) -> None:
-        self.next: Menu | None = None
+class ScrollingTextMenu(Menu):
+    def __init__(
+        self, manager: MenuManager, title: str, lines: list[str] = []
+    ) -> None:
+        super().__init__(manager, title)
+        self.lines: list[str] = lines
+        self.offset: int = 0
+        self.base_line: int = -1
 
-    def start(self, starting_menu: Menu) -> None:
-        starting_menu.display()
+    def render_lines(self) -> None:
+        curses.move(self.base_line, 0)
+        for line in self.lines[
+            self.offset : self.offset + constants.SCROLLING_TEXT_SIZE
+        ]:
+            curses.addstr(constants.CONTENT_PADDING*" " + line)
+            curses.addch("\n")
+        curses.refresh()
 
-        while self.next:
-            next = self.next
-            self.next = None
-            next.display()
+    def scroll_up(self, by: int = 1) -> None:
+        self.offset = max(0, self.offset - by)
+        self.render_lines()
 
-    def queue_menu(self, menu: Menu) -> None:
-        self.next = menu
+    def scroll_down(self, by: int = 1) -> None:
+        self.offset = min(
+            len(self.lines) - constants.SCROLLING_TEXT_SIZE, self.offset + by
+        )
+        self.render_lines()
+
+    def extra_input(self, key: str) -> bool:
+        if key == constants.SCROLL_UP_KEY:
+            self.scroll_up()
+        elif key == constants.SCROLL_DOWN_KEY:
+            self.scroll_down()
+
+        return True
+
+    def render_content(self) -> None:
+        self.base_line = curses.getyx(termui.stdscr)[0]
+        self.render_lines()
+        curses.addch("\n")
+        curses.addstr(
+            f"Press [{constants.SCROLL_DOWN_KEY}] "
+            f"or [{constants.SCROLL_UP_KEY}] to go up or down, respectively\n"
+        )
+        curses.addch("\n")
